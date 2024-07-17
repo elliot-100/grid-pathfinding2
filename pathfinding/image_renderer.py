a1320fcee3faf1cc7124283c8dd9ee5585f13baa
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-07-17 18:11:50 +0100

    feat: simple static graphical renderer

---
+++ pathfinding/image_renderer.py
@@ -0,0 +1,64 @@
+"""SimpleRenderer class module."""
+
+from typing import ClassVar
+
+from PIL import Image
+
+from .grid import Grid
+from .grid_ref import GridRef
+
+
+class GridRenderer:
+    """Render the grid as a static image."""
+
+    COLOR_MAPPING: ClassVar = {
+        "EMPTY": (128, 128, 128),  # pygame 'grey50'
+        "BLOCK": (104, 104, 104),  # pygame 'grey50'
+        "START": (0, 139, 0),  # pygame 'green4'
+        "GOAL": (238, 0, 0),  # pygame 'red2'
+        "ON_PATH": (255, 193, 37),  # pygame 'goldenrod1'
+    }
+
+    def __init__(
+        self,
+        grid: Grid,
+        scale: int = 32,
+    ) -> None:
+        """Create a new GridRenderer instance."""
+        self.grid = grid
+        self._image = Image.new(
+            mode="RGB",  # 3x8-bit pixels, true color
+            size=(self.grid.size_x, self.grid.size_y),
+        )
+        # populate pixels
+        pixels = [
+            self._pixel_color(x, y)
+            for x in range(self.grid.size_x)
+            for y in range(self.grid.size_y)
+        ]
+        self._image.putdata(pixels)
+        # resize for output
+        self._image = self._image.resize(
+            size=(scale * grid.size_x, scale * self.grid.size_y),
+            resample=Image.Resampling.NEAREST,
+        )
+
+    def _pixel_color(self, x: int, y: int) -> tuple[int, int, int]:
+        """Determine the pixel colour."""
+        location = GridRef(x, y)
+        for agent in self.grid.agents:
+            if location == agent.goal:
+                return self.COLOR_MAPPING["GOAL"]  # type: ignore[no-any-return]
+            if location == agent.location:
+                return self.COLOR_MAPPING["START"]  # type: ignore[no-any-return]
+            if location in agent.path_to_goal:
+                return self.COLOR_MAPPING["ON_PATH"]  # type: ignore[no-any-return]
+        if location in self.grid.untraversable_locations:
+            return self.COLOR_MAPPING["BLOCK"]  # type: ignore[no-any-return]
+        return self.COLOR_MAPPING["EMPTY"]  # type: ignore[no-any-return]
+
+    def show(
+        self,
+    ) -> None:
+        """Show the image."""
+        self._image.show()
